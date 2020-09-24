4a8986f176084e63aa8ea43d55f8a886b29b85a5
alxdncn <alex.duncan@lookingglassfactory.com>
2020-09-24 09:10:38 -0400

    Starting on new orchid project

---
+++ Assets/SetLineRendererDynamically.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetLineRendererDynamically : MonoBehaviour {
+
+	Vector3[] childrenNodes;
+
+	[SerializeField] LineRenderer lineRenderer;
+
+	// Use this for initialization
+	void Start () {
+		int children = transform.childCount;
+		childrenNodes = new Vector3[children];
+		lineRenderer.positionCount = children + 1;
+		for(int i = 0; i < children; i++){
+			childrenNodes[i] = transform.GetChild(i).GetComponent<Transform>().position;
+			lineRenderer.SetPosition(i, childrenNodes[i]);
+		}
+		lineRenderer.SetPosition(children, childrenNodes[0]);
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
---
+++ Assets/TraceMouseMovement.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TraceMouseMovement : MonoBehaviour {
+
+	LineRenderer lineRenderer;
+
+	Vector3 delta;
+	Vector3 lastMousePos;
+	Vector3 currentMousePos;
+
+	[SerializeField] float changeThreshold = 0.1f;
+
+	[SerializeField] int positionsToHold = 10;
+
+	// Use this for initialization
+	void Start () {
+		lineRenderer = GetComponent<LineRenderer>();
+
+		lineRenderer.positionCount = positionsToHold;
+
+		for(int i = 0; i < positionsToHold; i++){
+			lineRenderer.SetPosition(i, Input.mousePosition);
+		}
+
+		lastMousePos = Input.mousePosition;
+		lastMousePos.z = 0;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 currentScreenPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		currentScreenPos.z = 0;
+
+		if(Input.GetMouseButtonDown(0)){
+			for(int i = 0; i < positionsToHold; i++){
+				lineRenderer.SetPosition(i, currentScreenPos);
+			}
+		} else if(Input.GetMouseButton(0)){
+			currentMousePos = Input.mousePosition;
+			currentMousePos.z = 0;
+
+			delta = currentMousePos - lastMousePos;
+
+			lastMousePos = currentMousePos;
+
+			
+
+			if(delta.magnitude >= changeThreshold){
+				SetMousePositions();
+				lineRenderer.SetPosition(positionsToHold - 1, currentScreenPos);
+			}
+		} else if(DifferenceBetweenLinePoints() > 0.01f){
+			SetMousePositions();
+		}
+	}
+
+	void SetMousePositions(){
+		for(int i = 0; i < positionsToHold - 1; i++){
+			lineRenderer.SetPosition(i, lineRenderer.GetPosition(i + 1));
+		}
+	}
+
+	float DifferenceBetweenLinePoints(){
+		float diff = 0;
+		for(int i = 0; i < positionsToHold - 1; i++){
+			diff += Vector3.Magnitude(lineRenderer.GetPosition(i) - lineRenderer.GetPosition(i + 1));
+		}
+
+		return diff;
+	}
+}
